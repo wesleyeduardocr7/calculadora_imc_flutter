ca5dca9e62a4ea8646b9f85a634b2f92b5f68fa1
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-05-14 14:13:17 -0300

    finalizando_design_tela

--- lib/main.dart
+++ lib/main.dart
@@ -25,33 +25,52 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          TextField(
-            keyboardType: TextInputType.number,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(labelText: "Peso: (Kg)"),
-            cursorColor: Colors.purple,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(labelText: "Altura: (cm)"),
-            cursorColor: Colors.purple,
-          ),
 
-          RaisedButton(
-            child: Text("Calcular",
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-            onPressed: (){},
-          ),
+      body: SingleChildScrollView(
 
-        ],
-      ),
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+            TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(labelText: "Peso: (Kg)"),
+              cursorColor: Colors.purple,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(labelText: "Altura: (cm)"),
+              cursorColor: Colors.purple,
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 10.0),
+              child:   Container(
+                height: 50.0,
+                child: RaisedButton(
+                  color: Colors.green,
+                  child: Text("Calcular",
+                      style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold)),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text("Info",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0))
+            ),
+          ],
+        ),
+      ) ,
     );
   }
 }
